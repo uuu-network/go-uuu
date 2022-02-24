8660824544ca9ebd84739d6fd10ec9ddeb369bb3
uuu-network <82502856+uuu-network@users.noreply.github.com>
2022-02-24 13:45:26 +0800

    fix nonce for pending accounts

--- graphql/graphql_test.go
+++ graphql/graphql_test.go
@@ -20,6 +20,7 @@ import (
 	"testing"
 )
 
+
 func TestBuildSchema(t *testing.T) {
 	// Make sure the schema can be parsed and matched up to the object model.
 	if _, err := newHandler(nil); err != nil {
